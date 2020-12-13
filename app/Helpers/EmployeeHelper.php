8f5f636b632c615c69cbbfd8497eba6566b06ace
Alvin Velazquez <alvin.velazquez@kunn.es>
2020-12-13 13:15:53 +0100

    Created EmployeeHelper

---
+++ app/Helpers/EmployeeHelper.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Helpers;
+
+use App\Helpers\Util;
+use App\Models\Employee;
+use DB;
+use Illuminate\Http\Request;
+
+class EmployeeHelper {
+
+	/*
+	 * Recoger registros de un exporto hipotecario
+     */
+	public static function getRecords($employee_id){
+		$current_minutes = Util::time_to_minutes(date('H:i'));
+        
+        $employee = Employee::findOrFail($employee_id);
+        $records = $employee->records()
+                    ->select(
+                        'id',
+                        'full_name',
+                        'email',
+                        'phone_number',
+                        'income',
+                        'requested_amount',
+                        'time_slot_start',
+                        'time_slot_end',
+                        DB::raw('(requested_amount/income)* TIMESTAMPDIFF(HOUR,created_at,CURRENT_TIMESTAMP) as scoring') 
+                    )
+                    ->where([
+                        ['time_slot_start','<=', $current_minutes],
+                        ['time_slot_end','>', $current_minutes]
+                    ])
+                    ->orderBy('scoring', 'desc')
+                    ->get();
+
+        return $records;
+	}
+}
--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -3,36 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Employee;
-use Validator;
-use DB;
-use App\Helpers\Util;
+use App\Helpers\EmployeeHelper;
 
 class EmployeeController extends Controller
 {
     public function getRecords($employee_id) {
-    	$current_minutes = Util::time_to_minutes(date('H:i'));
-    	
-    	$employee = Employee::findOrFail($employee_id);
-    	$records = $employee->records()
-    				->select(
-    					'id',
-    					'full_name',
-    					'email',
-    					'phone_number',
-    					'income',
-    					'requested_amount',
-    					'time_slot_start',
-    					'time_slot_end',
-    					DB::raw('(requested_amount/income)* TIMESTAMPDIFF(HOUR,created_at,CURRENT_TIMESTAMP) as scoring') 
-    				)
-    				->where([
-    					['time_slot_start','<=', $current_minutes],
-    					['time_slot_end','>', $current_minutes]
-    				])
-    				->orderBy('scoring', 'desc')
-    				->get();
-
-    	return $records;
+    	return EmployeeHelper::getRecords($employee_id);
     }
 }
